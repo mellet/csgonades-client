416a0c7e15460a7b81168642edcd8e101f965e78
Mellet Solbakk <mellet89@gmail.com>
2020-05-28 23:04:19 +0200

    Package tweaks

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const base = {
   typescript: {
     ignoreDevErrors: true,
   },
 };
+
+module.exports = withBundleAnalyzer(base);
