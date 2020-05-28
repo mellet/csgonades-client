0273c29d84137826e158f7a7ee60a875823c7368
Mellet Solbakk <mellet89@gmail.com>
2020-05-28 07:53:23 +0200

    Random affiliate

--- src/common/adunits/AffUnit.tsx
+++ src/common/adunits/AffUnit.tsx
@@ -1,15 +1,44 @@
 import { FC } from "react";
 
+const units = [
+  {
+    name: "kinguin",
+    link: "https://www.tkqlhce.com/click-8411959-14050302",
+    img: "https://www.tqlkg.com/image-8411959-14050302",
+    height: 250,
+    width: 300,
+    alt: "Promote & earn money",
+  },
+  {
+    name: "dayton",
+    link: "https://www.anrdoezrs.net/click-8411959-14039103",
+    img: "https://www.lduhtrp.net/image-8411959-14039103",
+    height: 250,
+    width: 250,
+    alt: "Dayton CSA Work Boots",
+  },
+  {
+    name: "lootbox",
+    link: "https://www.tkqlhce.com/click-8411959-13902096",
+    img: "https://www.awltovhc.com/image-8411959-13902096",
+    width: 300,
+    height: 250,
+    alt: "Get the #1 monthly mystery box for geeks & gamers!",
+  },
+];
+
 export const AffUnit: FC = ({}) => {
+  const randomAff = units[Math.floor(Math.random() * units.length)];
+
   return (
     <>
       <div className="aff-unit">
-        <a href="https://www.tkqlhce.com/click-8411959-14050302" target="_top">
+        <a href={randomAff.link} target="_top">
           <img
-            src="https://www.tqlkg.com/image-8411959-14050302"
-            width="300"
-            height="250"
-            alt="Promote & earn money"
+            src={randomAff.img}
+            width={randomAff.width}
+            height={randomAff.height}
+            alt={randomAff.alt}
           />
         </a>
       </div>
