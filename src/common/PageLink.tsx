19c6c5926feefe096337961546d1901726563f31
Mellet Solbakk <mellet89@gmail.com>
2020-09-21 20:11:28 +0200

    Fix errors

--- src/common/PageLink.tsx
+++ src/common/PageLink.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 import Link from "next/link";
 
-export const PageLink = forwardRef<Link, any>(
+export const PageLink = forwardRef<typeof Link, any>(
   ({ children, href, as, ...rest }, ref) => {
     return (
       <Link href={href} as={as}>
