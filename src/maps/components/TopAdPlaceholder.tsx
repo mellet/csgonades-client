7f4417c27cb92662fc7152e885a5314100c36a0a
Mellet Solbakk <mellet89@gmail.com>
2020-09-28 23:49:46 +0200

    Fixed ad spot on map page to avoid page jumping

--- src/maps/components/TopAdPlaceholder.tsx
+++ src/maps/components/TopAdPlaceholder.tsx
@@ -1,21 +1,28 @@
 import { FC } from "react";
 import { EzoicPlaceholder } from "../../common/adunits/EzoicPlaceholder";
+import { PageCentralize } from "../../common/PageCentralize";
 import { Dimensions } from "../../constants/Constants";
+import { useTheme } from "../../store/SettingsStore/SettingsHooks";
 
 export const TopAdPlaceholder: FC = () => {
+  const { colors } = useTheme();
+
   return (
     <>
-      <div className="ph-top">
-        <EzoicPlaceholder id="175" />
-      </div>
+      <PageCentralize>
+        <div className="ph-top">
+          <EzoicPlaceholder id="175" />
+        </div>
+      </PageCentralize>
       <style jsx>{`
         .ph-top {
           display: flex;
           justify-content: space-around;
           align-items: center;
-          max-width: ${Dimensions.PAGE_WIDTH}px;
-          margin: 0 auto;
-          padding-top: ${Dimensions.GUTTER_SIZE}px;
+          margin-top: ${Dimensions.GUTTER_SIZE / 2}px;
+          min-height: 90px;
+          background: ${colors.DP01};
+          border-radius: 5px;
         }
       `}</style>
     </>
