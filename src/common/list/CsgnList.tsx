c5631a379e00f2812b9da7d3598f24f73ee74686
Mellet Solbakk <mellet89@gmail.com>
2020-10-11 15:05:13 +0200

    Ad bg in content

--- src/common/list/CsgnList.tsx
+++ src/common/list/CsgnList.tsx
@@ -82,6 +82,9 @@ const List: FC<Props<any>> = memo(
             display: flex;
             align-items: center;
             justify-content: center;
+            background: ${colors.DP02};
+            border-radius: ${Dimensions.BORDER_RADIUS};
+            margin-bottom: ${Dimensions.GUTTER_SIZE}px;
           }
 
           @media only screen and (max-width: 1020px) {
