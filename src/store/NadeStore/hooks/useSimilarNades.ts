4687a6cf02f705484ccc458f871952c13d75424f
Mellet Solbakk <mellet89@gmail.com>
2020-11-24 23:38:23 +0100

    Remove more unused code

--- src/store/NadeStore/hooks/useSimilarNades.ts
+++
@@ -1,9 +0,0 @@
-import { Nade } from "../../../nade-data/Nade/Nade";
-
-export const useSimilarNades = (nade: Nade) => {
-  // TODO: Implement similar nades path on api
-
-  console.log("> Not implemented useSimilarNades", nade.id);
-
-  return [];
-};
