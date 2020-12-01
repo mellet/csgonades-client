1e375323a57063531e062306fb617e48930f16e8
Mellet Solbakk <mellet89@gmail.com>
2020-12-01 23:49:50 +0100

    Memoice clustering function

--- src/store/MapStore/hooks/useNadesForMapView.ts
+++ src/store/MapStore/hooks/useNadesForMapView.ts
@@ -59,9 +59,28 @@ export const useNadeClusters = (nades: NadeLight[]): NadeLight[][] => {
       return [];
     }
 
-    const cluster = custerNades(nades);
+    const cluster = memClusterNades(nades);
     return cluster;
   }, [nades]);
 
   return unqiueNadesForPosition;
 };
+
+const memoizeClusterNades = (
+  fn: (nades: NadeLight[]) => NadeLight[][]
+): ((nades: NadeLight[]) => NadeLight[][]) => {
+  const cache = {};
+  return (...args) => {
+    const nades = args[0] as NadeLight[];
+    const key = nades.reduce((acc, nade) => (acc += nade.id), "");
+    if (key in cache) {
+      return cache[key];
+    } else {
+      const result = fn(nades);
+      cache[key] = result;
+      return result;
+    }
+  };
+};
+
+const memClusterNades = memoizeClusterNades(custerNades);
