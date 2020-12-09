d660f24bef1aa4f866b71d6fdb89b9933b3bdc76
Mellet Solbakk <mellet89@gmail.com>
2020-12-09 17:32:16 +0100

    Add reset button when nades are empty

--- src/maps/components/NoNadesMessage.tsx
+++ src/maps/components/NoNadesMessage.tsx
@@ -1,8 +1,10 @@
 import { FC, memo } from "react";
 import { Dimensions } from "../../constants/Constants";
+import { useFilterReset } from "../../store/MapStore/hooks/useFilterReset";
 import { useTheme } from "../../store/SettingsStore/SettingsHooks";
 
 export const NoNadesMessage: FC = memo(() => {
+  const { resetFilter } = useFilterReset();
   const { colors } = useTheme();
 
   return (
@@ -14,6 +16,9 @@ export const NoNadesMessage: FC = memo(() => {
           <br />
           Click the button + in the corner.
         </p>
+        <button className="reset" onClick={resetFilter}>
+          Reset Filter
+        </button>
       </div>
       <style jsx>{`
         .no-nades-msg {
@@ -22,6 +27,8 @@ export const NoNadesMessage: FC = memo(() => {
           overflow: hidden;
           color: ${colors.TEXT};
           border: 1px solid ${colors.BORDER};
+          display: flex;
+          flex-direction: column;
         }
 
         h3 {
@@ -30,12 +37,28 @@ export const NoNadesMessage: FC = memo(() => {
           border-bottom: 1px solid ${colors.BORDER};
           margin: 0;
           font-size: 20px;
+          text-align: center;
         }
 
         p {
           padding: 10px ${Dimensions.GUTTER_SIZE}px;
           margin: 0;
           font-size: 16px;
+        }
+
+        .reset {
+          margin: ${Dimensions.GUTTER_SIZE}px;
+          border-radius: ${Dimensions.BORDER_RADIUS};
+          background: ${colors.primaryBtnBg};
+          color: white;
+          border: none;
+          padding: 10px 16px;
+          cursor: pointer;
+          transition: background 0.2s;
+        }
+
+        .reset:hover {
+          background: ${colors.primaryBtnHover};
         }
       `}</style>
     </>
