11023636edda50e3beba15d1a7f6fe26ffbc2165
Mellet Solbakk <mellet89@gmail.com>
2021-02-06 19:41:42 +0100

    Log favorite actions

--- src/favorites/components/FavoriteButton.tsx
+++ src/favorites/components/FavoriteButton.tsx
@@ -9,6 +9,7 @@ import { useUnfavorite } from "../data/hooks/useUnFavorite";
 import { IconButton } from "../../shared-components/buttons/IconButton";
 import { useTheme } from "styled-components";
 import { Tooltip } from "../../shared-components/Tooltip/Tooltip";
+import { useAnalytics } from "../../utils/Analytics";
 
 type Props = {
   nadeId: string;
@@ -17,6 +18,7 @@ type Props = {
 
 export const FavoriteButton: FC<Props> = ({ nadeId, favoriteCount }) => {
   const { colors } = useTheme();
+  const { event } = useAnalytics();
   const [internalFavCount, setInternalFavoriteCount] = useState(favoriteCount);
   const { setSignInWarning } = useSignInWarning();
   const isFavoriteInProgress = useIsFavoriteInProgress();
@@ -46,10 +48,20 @@ export const FavoriteButton: FC<Props> = ({ nadeId, favoriteCount }) => {
     }
 
     if (favorite) {
+      event({
+        category: "Favoirte",
+        action: "Remove",
+        label: nadeId,
+      });
       unFavorite(favorite.id);
       setInternalFavoriteCount(internalFavCount - 1);
       setOptimisticIsFavorited(false);
     } else {
+      event({
+        category: "Favoirte",
+        action: "Add",
+        label: nadeId,
+      });
       addFavorite(nadeId);
       setInternalFavoriteCount(internalFavCount + 1);
       setOptimisticIsFavorited(true);
