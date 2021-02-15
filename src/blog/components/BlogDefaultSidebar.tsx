708e9a821a5a82de3025743e2a806d3db91df103
Mellet Solbakk <mellet89@gmail.com>
2021-02-15 01:43:17 +0100

    Sticky blog sidebar ad

--- src/blog/components/BlogDefaultSidebar.tsx
+++ src/blog/components/BlogDefaultSidebar.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { SidebarSkyskraperAd } from "../../shared-components/adunits/SidebarSkyskraper";
 import { NadeShareActions } from "../../nade/components/NadeShareActions";
 import { BlogPost } from "../models/BlogPost";
+import { Dimensions } from "../../constants/Constants";
 
 type Props = {
   data: BlogPost;
@@ -22,6 +23,18 @@ export const BlogDefaultSidebar: FC<Props> = ({ data }) => {
       <div id="blog-sidebar-wrap" className="spacer">
         <SidebarSkyskraperAd />
       </div>
+      <style jsx>{`
+        #blog-share {
+          border-radius: 8px;
+          overflow: hidden;
+        }
+
+        #blog-sidebar-wrap {
+          margin-top: ${Dimensions.GUTTER_SIZE}px;
+          position: sticky;
+          top: ${Dimensions.HEADER_HEIGHT + Dimensions.GUTTER_SIZE}px;
+        }
+      `}</style>
     </>
   );
 };
