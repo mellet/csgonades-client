346bc8bebf6832f3a1f6e0b89d5fe7ed8187e8bd
Mellet Solbakk <mellet89@gmail.com>
2021-03-31 21:43:28 +0200

    Use new SVG icons (#58)

---
+++ src/shared-components/nade-icons/NadeIcon.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import { NadeType } from "../../nade/models/NadeType";
+import { SmokeIcon } from "./SmokeIcon";
+import { FlashIcon } from "./FlashIcon";
+import { MolotovIcon } from "./MolotovIcon";
+import { HEIcon } from "./HEIcon";
+import { assertNever } from "../../utils/Common";
+
+type Props = {
+  nadeType: NadeType;
+  size?: number;
+};
+
+export const NadeIcon: FC<Props> = ({ nadeType, size }) => {
+  switch (nadeType) {
+    case "smoke":
+      return <SmokeIcon size={size} />;
+    case "flash":
+      return <FlashIcon size={size} />;
+    case "molotov":
+      return <MolotovIcon size={size} />;
+    case "hegrenade":
+      return <HEIcon size={size} />;
+    default:
+      assertNever(nadeType);
+      return null;
+  }
+};
