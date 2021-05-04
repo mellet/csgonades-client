ad1775a6adfca74e640d73268b43704b456e111c
Mellet Solbakk <mellet89@gmail.com>
2021-05-05 00:18:03 +0200

    Use swr for map nades

---
+++ src/nade/data/useMapNades.ts
@@ -0,0 +1,29 @@
+import useSWR from "swr";
+import { CsgoMap } from "../../map/models/CsGoMap";
+import { NadeLight } from "../models/Nade";
+import { NadeApi } from "./NadeApi";
+
+async function fetcher(map: CsgoMap) {
+  console.log("# Refetching nades", map);
+  const mapNadesResult = await NadeApi.getByMap(map);
+
+  if (mapNadesResult.isOk()) {
+    return mapNadesResult.value;
+  } else {
+    throw Error(mapNadesResult.error.message);
+  }
+}
+
+export const useMapNades = (initialNades: NadeLight[], map: CsgoMap) => {
+  const { data, error } = useSWR(map, fetcher, {
+    initialData: initialNades,
+    revalidateOnMount: false,
+    focusThrottleInterval: 60 * 1000,
+    errorRetryCount: 3,
+  });
+
+  return {
+    nades: data,
+    error,
+  };
+};
