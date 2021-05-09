1253ad4bb4264db5d6e9331200000467a0457917
Mellet Solbakk <mellet89@gmail.com>
2021-05-09 19:49:39 +0200

    Fix token not refreshing when fetching notifications

--- src/notification/data/hooks/useNotification.ts
+++ src/notification/data/hooks/useNotification.ts
@@ -1,10 +1,20 @@
 import { useCallback } from "react";
 import useSWR, { mutate } from "swr";
-import { useToken } from "../../../core/authentication/useGetToken";
+import {
+  useGetOrUpdateToken,
+  useToken,
+} from "../../../core/authentication/useGetToken";
 import { NotificationApi } from "../NotificationApi";
 
-async function fetcher(_key: string, token: string) {
-  console.log("#Fetching notification");
+async function fetcher(_key: string, fetchToken: () => Promise<string | null>) {
+  console.log("# Getting token to fetch notifications", new Date());
+  const token = await fetchToken();
+
+  if (!token) {
+    return;
+  }
+
+  console.log("#Fetching notification", new Date());
   const result = await NotificationApi.getNotifications(token);
 
   if (result.isOk()) {
@@ -17,10 +27,17 @@ async function fetcher(_key: string, token: string) {
 export const useRawNotifications = () => {
   const notificationFetchDelay = 5 * 60 * 1000;
   const token = useToken();
+  const fetchToken = useGetOrUpdateToken();
 
-  const { data } = useSWR(token ? ["/notifications", token] : null, fetcher, {
-    dedupingInterval: notificationFetchDelay,
-  });
+  const { data } = useSWR(
+    token ? ["/notifications", fetchToken] : null,
+    fetcher,
+    {
+      dedupingInterval: notificationFetchDelay,
+      revalidateOnFocus: true,
+      focusThrottleInterval: notificationFetchDelay,
+    }
+  );
 
   const markAsViewed = useCallback(async () => {
     if (!token || !data) {
