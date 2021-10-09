db856d50193d80ccf4b98724cdb8124f3768aec5
Mellet Solbakk <mellet89@gmail.com>
2021-10-09 14:52:22 +0200

    Fix client id and slot for google adsense

--- src/shared-components/adunits/Adsense.tsx
+++ src/shared-components/adunits/Adsense.tsx
@@ -4,8 +4,8 @@ import AdSense from "react-ssr-adsense";
 export const AdUnitAdSense: FC = () => {
   return (
     <AdSense
-      client="ca-pub-7292810486004926"
-      slot="7806394673"
+      client="ca-pub-2255854420599519"
+      slot="1671636402"
       style={{ display: "block" }}
       layout="in-article"
       format="fluid"
