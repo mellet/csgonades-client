67d7dcc9266543fd66daeaa95cd6bfb1b02d2573
Mellet Solbakk <mellet89@gmail.com>
2021-12-26 18:38:45 +0100

    Ad max size tweak

--- src/shared-components/adunits/AdUnit.tsx
+++ src/shared-components/adunits/AdUnit.tsx
@@ -1,6 +1,6 @@
 import { FC, memo, useRef } from "react";
 import { useInViewport } from "react-in-viewport";
-import { useMediaQuery } from "react-responsive";
+import { useElementSize } from "usehooks-ts";
 import { Dimensions } from "../../constants/Constants";
 import { Adsense, AdUnitName } from "./Adsense";
 
@@ -10,29 +10,35 @@ type Props = {
 };
 
 export const AdUnit: FC<Props> = memo(({ name, horizontalSpacing }) => {
-  const isMobile = useMediaQuery({ maxWidth: 600 });
-  const myRef = useRef<HTMLDivElement>(null);
-  const { enterCount } = useInViewport(myRef, {
+  const [wrapRef, { width }] = useElementSize();
+  const adRef = useRef<HTMLDivElement>(null);
+  const { enterCount } = useInViewport(adRef, {
     rootMargin: "400px",
   });
 
   const renderAd = Boolean(enterCount);
 
+  const hasSize = width > 0;
+
   return (
     <>
-      <div
-        className="ad-unit"
-        ref={myRef}
-        style={{
-          marginTop: horizontalSpacing ? Dimensions.GUTTER_SIZE : 0,
-          marginBottom: horizontalSpacing ? Dimensions.GUTTER_SIZE : 0,
-        }}
-      >
-        {renderAd && <Adsense adName={name} />}
+      <div ref={wrapRef} className="ad-wrap">
+        {hasSize && (
+          <div
+            className="ad-unit"
+            ref={adRef}
+            style={{
+              marginTop: horizontalSpacing ? Dimensions.GUTTER_SIZE : 0,
+              marginBottom: horizontalSpacing ? Dimensions.GUTTER_SIZE : 0,
+            }}
+          >
+            {renderAd && <Adsense adName={name} />}
+          </div>
+        )}
       </div>
       <style jsx>{`
         .ad-unit {
-          max-width: ${isMobile ? "90vw" : "auto"};
+          max-width: ${width}px;
         }
 
         .ad-unit:after {
