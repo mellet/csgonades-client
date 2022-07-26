37dcc3631ae7d8fd97ad187850776540fb8591c0
Mellet Solbakk <mellet89@gmail.com>
2022-07-27 00:59:22 +0200

    Add tooltip to info button

--- src/nade/components/NadeInfoButton.tsx
+++ src/nade/components/NadeInfoButton.tsx
@@ -1,13 +1,22 @@
 import { FC } from "react";
 import { FaInfo } from "react-icons/fa";
 import { SquareButton } from "../../shared-components/buttons/IconButton/IconButton";
+import { Tooltip } from "../../shared-components/Tooltip/Tooltip";
 
 export const NadeInfoButton: FC = () => {
   return (
     <>
-      <a href="#description">
-        <SquareButton icon={<FaInfo />} active={false} activeColor={"teal"} />
-      </a>
+      <div className="nade-info">
+        <a href="#description">
+          <Tooltip message="Description" direction="right">
+            <SquareButton
+              icon={<FaInfo />}
+              active={false}
+              activeColor={"teal"}
+            />
+          </Tooltip>
+        </a>
+      </div>
       <style jsx>{``}</style>
     </>
   );
