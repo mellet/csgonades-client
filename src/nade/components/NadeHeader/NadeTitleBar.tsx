30cd7422829962e7caaf2862d0069086327cea80
Mellet Solbakk <mellet89@gmail.com>
2022-08-08 16:12:06 +0200

    Do not scroll on back button

--- src/nade/components/NadeHeader/NadeTitleBar.tsx
+++ src/nade/components/NadeHeader/NadeTitleBar.tsx
@@ -18,7 +18,11 @@ export const NadeTitleBar: FC<Props> = memo(({ nade }) => {
       <div className="title">
         <div id="left-controls">
           {!!nade.map && (
-            <Link href={`/maps/${nade.map}?type=${nade.type}`} passHref>
+            <Link
+              href={`/maps/${nade.map}?type=${nade.type}`}
+              passHref
+              scroll={false}
+            >
               <button id="back">
                 <FaChevronLeft style={{ position: "relative", left: "-2px" }} />
               </button>
