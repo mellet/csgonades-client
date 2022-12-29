31ad78e0a652cf07793eb907264fbf0a60c6bcbd
Mellet Solbakk <mellet89@gmail.com>
2022-12-28 18:57:41 -0800

    Improved sort

--- src/nade/models/Nade.ts
+++ src/nade/models/Nade.ts
@@ -68,6 +68,7 @@ export interface NadeLight {
   imageMain: NadeImageData;
   isFavorited?: boolean;
   isPro?: boolean;
+  isNew?: boolean;
   mapEndCoord?: MapCoordinates;
   movement?: Movement;
   oneWay?: boolean;
