0860b66ef700b2b2c7996ac009caf37504308615
Mellet Solbakk <mellet89@gmail.com>
2023-06-19 20:37:34 -0700

    Make battle royal more clickable

--- src/map/components/EloGame/EloGameButton.tsx
+++ src/map/components/EloGame/EloGameButton.tsx
@@ -20,16 +20,23 @@ export const EloGameButton: FC<Props> = ({ onClick }) => {
           display: flex;
           align-items: center;
           justify-content: center;
-          transition: background 0.1s;
+          transition: opacity 0.1s;
           padding: 10px;
           height: 32px;
           font-size: 14px;
           font-weight: 500;
+          opacity: 0.9;
+          background: rgb(255, 254, 0);
+          background: linear-gradient(
+            90deg,
+            rgba(255, 254, 0, 1) 0%,
+            rgba(255, 194, 0, 1) 50%
+          );
         }
 
         button:hover {
           cursor: pointer;
-          background: rgba(255, 255, 255, 1);
+          opacity: 1;
         }
       `}</style>
     </>
