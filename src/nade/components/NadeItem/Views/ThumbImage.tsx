f25ee0dbaecea06b6702e111893e4c5e5e055457
Mellet Solbakk <mellet89@gmail.com>
2023-08-18 01:46:59 -0700

    Tweak nade item images

--- src/nade/components/NadeItem/Views/ThumbImage.tsx
+++ src/nade/components/NadeItem/Views/ThumbImage.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import { FC, memo, useEffect, useState } from "react";
 import { CrossHairSvg } from "../../VideoContainer/CrossHairSvg";
 import { useTheme } from "../../../../core/settings/useTheme";
-import { FaVideo } from "react-icons/fa";
-import { CSGNIcon } from "../../NadeStatus/CSGNIcon";
 
 type Props = {
   lineupThumbUrl?: string;
   thumbUrl?: string;
 };
+
+const LINEUP_SIZE = 100;
 
 export const ThumbImage: FC<Props> = memo(({ lineupThumbUrl, thumbUrl }) => {
   const hasLineUpImage = !!lineupThumbUrl;
@@ -46,61 +46,44 @@ export const ThumbImage: FC<Props> = memo(({ lineupThumbUrl, thumbUrl }) => {
   return (
     <>
       <div className={visible ? "thumb-image visible" : "thumb-image"}>
-        <div className="video-icon">
-          <CSGNIcon size={12} icon={<FaVideo color="rgba(0, 0, 0, 0.4)" />} />
-        </div>
-        <div className="result-image">
-          {thumbUrl && (
+        <div className="image-container">
+          {lineupThumbUrl && (
             <Image
-              alt="Nade result image"
               fill
-              onLoad={onResultImgRdy}
-              src={thumbUrl}
+              alt="Nade lineup image"
+              unoptimized
+              onLoad={onLineupImgRdy}
+              src={lineupThumbUrl}
+              quality={100}
               style={{
                 objectFit: "cover",
               }}
-            ></Image>
+            />
           )}
+          <div className="crosshair">
+            <CrossHairSvg size={20} />
+          </div>
         </div>
-        {lineupThumbUrl && (
+        {thumbUrl && (
           <>
-            <div className="lineup-border"></div>
             <div className="lineup-image">
               <div className="lineup-img-wrap">
                 <Image
+                  alt="Nade result image"
                   fill
-                  alt="Nade lineup image"
-                  unoptimized
-                  onLoad={onLineupImgRdy}
-                  src={lineupThumbUrl}
-                  quality={100}
+                  onLoad={onResultImgRdy}
+                  src={thumbUrl}
                   style={{
                     objectFit: "cover",
+                    transform: "scale(1.6)",
                   }}
-                />
-              </div>
-              <div className="crosshair">
-                <CrossHairSvg size={20} />
+                ></Image>
               </div>
             </div>
           </>
         )}
       </div>
       <style jsx>{`
-        .video-icon {
-          position: absolute;
-          top: 6px;
-          right: 6px;
-          z-index: 1;
-          color: #111;
-          background: rgba(255, 255, 255, 0.2);
-          padding: 3px 6px;
-          border-radius: 4px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-
         .thumb-image {
           opacity: 0;
           height: 100%;
@@ -113,33 +96,24 @@ export const ThumbImage: FC<Props> = memo(({ lineupThumbUrl, thumbUrl }) => {
           opacity: 1;
         }
 
-        .result-image {
+        .image-container {
           background: ${colors.DP01};
           align-items: center;
           display: flex;
           height: 100%;
           justify-content: center;
-          transform: translateX(${hasLineUpImage ? "-17%" : 0});
           width: 100%;
         }
 
-        .lineup-border {
-          background: ${colors.DP01};
-          bottom: 0;
-          clip-path: polygon(10% 0%, 100% 0%, 100% 100%, 0% 100%);
-          position: absolute;
-          right: 0;
-          top: 0;
-          width: 40.5%;
-        }
-
         .lineup-image {
-          bottom: 0;
-          clip-path: polygon(10% 0%, 100% 0%, 100% 100%, 0% 100%);
+          width: ${LINEUP_SIZE}px;
+          height: ${LINEUP_SIZE * (9 / 14)}px;
+          border-radius: 12px;
           position: absolute;
-          right: 0;
-          top: 0;
-          width: 40%;
+          bottom: 12px;
+          left: 12px;
+          border: 2px solid white;
+          overflow: hidden;
         }
 
         .crosshair {
